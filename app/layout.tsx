675dfb0a3df76c4b377220486d9e22f51db270c5
nthapa0000 <21bma029@nith.ac.in>
2023-08-03 16:01:02 +0530

    Providers for auth and Supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
 
 const font = Figtree({ subsets: ['latin'] })
 
@@ -13,14 +15,19 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
+  
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
         <Sidebar>
         {children}
         </Sidebar>
+        </UserProvider>
+        </SupabaseProvider>
         </body>
     </html>
   )
